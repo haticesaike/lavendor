62552fe653ae11d0c15bb4da430939cf56cba307
Hatice Saike <haticesaike374@gmail.com>
2024-04-18 20:13:59 +0300

    leftSide done

--- src/components/LeftSideBar/LeftSideBar.tsx
+++ src/components/LeftSideBar/LeftSideBar.tsx
@@ -3,14 +3,21 @@ import SearchBar from "../SearchBar/SearchBar";
 import SidePanel from "../SidePanel/SidePanel";
 import UserInfo from "../UserInfo/UserInfo";
 import styles from "./LeftSideBar.module.css";
+import avatarW from "../../assets/avatar-w.svg";
 
 function LeftSideBar() {
+  const userInfoProps = {
+    name: "Ari Espinoza",
+    email: "espino@drafthouse.com",
+    avatar: avatarW,
+  };
   return (
     <div className={styles.leftSideBar}>
       <Navbar />
       <SearchBar />
       <SidePanel />
-      <UserInfo />
+      <div className={styles.divider}></div>
+      <UserInfo {...userInfoProps} />
     </div>
   );
 }
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,6 +1,25 @@
 import styles from "./UserInfo.module.css";
-function UserInfo() {
-  return <div>UserInfo</div>;
+import logOut from "../../assets/log-out.svg";
+
+interface UserInfoProps {
+  name: string;
+  email: string;
+  avatar: string;
 }
+
+const UserInfo: React.FC<UserInfoProps> = ({ name, email, avatar }) => {
+  return (
+    <div className={styles.userInfo}>
+      <img src={avatar} alt={`${name}'s avatar`} className={styles.avatar} />
+      <div className={styles.details}>
+        <div className={styles.nameWithIcon}>
+          <h5 className={styles.name}>{name}</h5>
+          <img src={logOut} alt="Expand" className={styles.logOut} />
+        </div>
+        <p className={styles.email}>{email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default UserInfo;
